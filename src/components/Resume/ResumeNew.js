c48f07cdfbd772cd3c9b8e2a120bc0bcc5bfb59b
heather-tech <heather.k.tech@gmail.com>
2023-11-11 11:54:50 -0500

    updates

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Heather.Kruszewski.Resume.pdf";
+// import pdf from "../../Assets/../Assets/Heather.Kruszewski.Resume.pdf";
+import pdf from "/Users/heather/Desktop/Portfolio/Portfolio/src/Assets/Heather.Kruszewski.Resume.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
