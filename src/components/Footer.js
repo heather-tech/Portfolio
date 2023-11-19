2ad3dbedf69d61b7a55ea6b0dbbe629aceadcda6
heather-tech <heather.k.tech@gmail.com>
2023-11-19 16:58:43 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import {
   AiOutlineTwitter,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
 
 function Footer() {
   return (
@@ -12,6 +13,16 @@ function Footer() {
       <Row>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
+          <li className="social-icons">
+              <a
+                href="heather.k.tech@gmail.com"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <IoIosMail />
+              </a>
+            </li>
             <li className="social-icons">
               <a
                 href="https://github.com/heather-tech"
